bc498052241bb349212a0130799caab4f8527733
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2026-04-13 18:32:48 -0300

    admin configurado

--- EAD/src/server.ts
+++ EAD/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { sequelize } from './database';
+import { adminJs, adminJsRouter } from './adminjs';
 
 
 const app = express();
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
+
+app.use(adminJs.options.rootPath, adminJsRouter);
 
 
 
@@ -13,5 +17,5 @@ app.listen(PORT, () => {
     }).catch((error) => {
         console.error('Unable to connect to the database:', error);
     });
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port localhost:${PORT}`);
 });
